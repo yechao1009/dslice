1b39d223b23f347c0b5c220f590319213c95695b
Chao Ye <yechao1009@gmail.com>
2017-03-25 16:43:25 +0800

    type define for dslice

---
+++ include/dslice/base.h
@@ -0,0 +1,15 @@
+#ifndef DSLICE_BASE_H_
+#define DSLICE_BASE_H_
+
+namespace dslice
+{
+
+typedef int ds_int;
+typedef unsigned ds_uint;
+typedef unsigned long ds_ulong;
+typedef float ds_float;
+typedef double ds_double;
+
+} // namespace
+
+#endif // DSLICE_BASE_H_
