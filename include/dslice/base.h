0ef12e5a44adc245c4eef80b2ca707e7921103cf
Chao Ye <yechao1009@gmail.com>
2017-04-16 12:35:47 +0800

    Add definition of struct

--- include/dslice/base.h
+++ include/dslice/base.h
@@ -1,14 +1,27 @@
 #ifndef DSLICE_BASE_H_
 #define DSLICE_BASE_H_
 
-namespace dslice
-{
+#include <vector>
+#include <string>
 
 typedef int ds_int;
 typedef unsigned ds_uint;
 typedef unsigned long ds_ulong;
 typedef float ds_float;
 typedef double ds_double;
+
+namespace dslice
+{
+
+struct Factor {
+	std::vector<ds_uint> factor;
+	ds_uint level;
+};
+
+struct DPTable {
+	ds_double score;
+	std::vector<ds_uint> strategy;
+};
 
 } // namespace
 
