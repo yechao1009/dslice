40bcbffb5a2d55af5b94b0aade45756853ce8d4f
Chao Ye <yechao1009@gmail.com>
2017-04-16 12:47:10 +0800

    use struct Factor

--- include/dslice/covariate.h
+++ include/dslice/covariate.h
@@ -3,17 +3,16 @@
 
 #include <iostream>
 #include <utility>
-#include <vector>
-#include <string>
 #include <map>
+#include <algorithm>
 #include "base.h"
 
 namespace dslice
 {
 
 template <typename Object>
-class Covariate
-{
+class Covariate {
+
 public:
 	Covariate(std::vector<Object> sample) {
 		sample_ = sample;
@@ -27,7 +26,13 @@ public:
 		GenerateFactor(as_factor);
 	}
 
-	Covariate (const Covariate<Object>& var) { *this = var; }
+	Covariate(std::vector<unsigned> sample, bool as_factor = true) {
+		sample_ = sample;
+		size_ = sample.size();
+		GenerateFactor(as_factor);
+	}
+
+	Covariate(const Covariate<Object>& var) { *this = var; }
 
 	void SetSize(ds_uint size) {
 		size_ = size;
@@ -37,7 +42,7 @@ public:
 		return sample_;
 	}
 	// fetch factor of input sample, begin with 0
-	std::vector<ds_uint> GetFactor() const {
+	dslice::Factor GetFactor() const {
 		return factor_;
 	}
 	// fetch sample to factor map
@@ -50,7 +55,7 @@ public:
 	}
 	// fecth factor level
 	ds_uint GetLevel() const {
-		return level_;
+		return factor_.level;
 	}
 	// reorder covariate according to given index
 	void Reorder(std::vector<ds_uint> index) {
@@ -61,45 +66,44 @@ public:
 			std::vector<ds_uint> fv;
 			for (ds_uint i = 0; i != size_; ++i) {
 				sv[i] = sample_[index[i]];
-				fv[i] = factor_[index[i]];
+				fv[i] = factor_.factor[index[i]];
 			}
 			sample_ = sv;
-			factor_ = sv;
+			factor_.factor = sv;
 		}
 	}
 
 private:
-
 	std::vector<Object> sample_;
-	std::vector<ds_uint> factor_;
+	Factor factor_;
 	std::map<Object, ds_uint> dict_;
 	ds_uint size_;
-	ds_uint level_;
+
 	// generate factor for non-integer input sample (e.g., string)
 	void GenerateFactor() {
 		dict_.clear();
-		factor_.resize(sample_.size());
+		factor_.factor.resize(sample_.size());
 		for (ds_uint i = 0; i != size_; ++i) {
 			dict_.insert(std::pair<Object, ds_uint>(sample_[i], dict_.size()));
-			factor_[i] = dict_[sample_[i]];
+			factor_.factor[i] = dict_[sample_[i]];
 		}
-		level_ = dict_.size();
+		factor_.level = dict_.size();
 	}
 	// generate factor for integer sample
 	void GenerateFactor(bool as_factor) {
 		dict_.clear();
-		factor_.resize(sample_.size());
+		factor_.factor.resize(sample_.size());
 		if (as_factor) {
 			for (ds_uint i = 0; i != size_; ++i) {
 				dict_.insert(std::pair<Object, ds_uint>(sample_[i], dict_.size()));
-				factor_[i] = dict_[sample_[i]];
+				factor_.factor[i] = dict_[sample_[i]];
 			}
-			level_ = dict_.size();
+			factor_.level = dict_.size();
 		} else {
 			for (ds_uint i = 0; i != size_; ++i) {
-				factor_[i] = (ds_uint)sample_[i];
+				factor_.factor[i] = (ds_uint)sample_[i];
 			}
-			level_ = *max_element(factor_.begin(), factor_.end()) + 1;
+			factor_.level = *max_element(factor_.factor.begin(), factor_.factor.end()) + 1;
 		}
 	}
 };
